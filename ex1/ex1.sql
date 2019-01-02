d4ea6e275b90bb2886b0c6dcfdfec6d7b9c8a90e
nikamanth appiah <nitinnikamanth@gmail.com>
2019-01-02 13:55:47 +0530

    ex2 start

--- ex1/ex1.sql
+++ ex1/ex1.sql
@@ -1,8 +1,18 @@
-drop table customer;
-drop table emp;
-drop table part;
-drop table orders;
 drop table order_details;
+drop table orders;
+drop table part;
+drop table emp;
+drop table customer;
+drop table pincode;
+
+rem:pincode
+
+create table pincode(
+pin number(6) constraint pin_pk primary key,
+loc char(15)
+);
+
+rem:emp
 
 create table emp(
 emp_no varchar2(15) constraint emp_pk primary key,
@@ -11,8 +21,7 @@ dob date,
 pin number(6) constraint emp_fk references pincode(pin),
 constraint emp_format check(emp_no like 'E%'));
 
-/////////////////
-
+rem:customer
 
 create table customer(
 cust_no varchar(10) constraint cus_pk primary key,
@@ -22,7 +31,7 @@ cus_phone number(10) constraint ph_no unique,
 dob date,
 constraint cus_format check(cust_no like 'C%'));
 
-////////////////
+rem:part
 
 create table part(
 p_no varchar2(15) constraint p_pk primary key,
@@ -31,7 +40,7 @@ price number(10) constraint p_par not null,
 qty number(10) constraint qty_ck check(qty>0),
 constraint part_format check (p_no like 'P%'));
 
-/////////////////
+rem:orders
 
 create table orders(
 o_no varchar2(5) constraint or_pk primary key,
@@ -41,7 +50,7 @@ rd date,
 sd date,
 constraint date_ck check(rd<sd));
 
-////////////////////
+rem:order_details
 
 create table order_details(
 o_no varchar2(5) constraint or_fk references orders(o_no),
@@ -49,7 +58,7 @@ p_no varchar2(15),
 qty number(10),
 constraint ord_pk primary key(o_no,p_no));
 
-////////////alter cmds:///////////////////
+rem: alter cmds:
 
 
 
@@ -64,10 +73,9 @@ alter table customer drop (dob);
 
 alter table orders modify rd date constraint ord_notnull not null;
 
-//alter table orders ondeletecascade;//not working
+rem:alter table orders ondeletecascade;//not working
 
-/*
+
 
 alter table order_details drop constraint or_fk;
 alter table order_details add constraint or_fk foreign key(o_no2) references orders(o_no) on delete cascade;
-*/
